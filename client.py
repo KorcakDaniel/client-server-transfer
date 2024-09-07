03bebcfbda3b1f39508cab0718cf9dccb1170f23
Daniel Korcak <13299392+KorcakDaniel@users.noreply.github.com>
2024-09-07 13:07:58 +0200

    Base model

---
+++ client.py
@@ -0,0 +1,113 @@
+import hashlib
+import logging
+import os
+import socket
+import time
+from typing import Optional
+
+from tqdm import tqdm
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+
+formatter = logging.Formatter("[%(name)s] %(asctime)s %(levelname)s - %(message)s")
+handler = logging.StreamHandler()
+logger.addHandler(handler)
+
+
+PORT = 3254
+CLIENT_DIR = "client"
+CHUNK_BYTES = 1024
+
+
+def check_directory() -> bool:
+    """Check if the specified directory exists or not"""
+    if not os.path.isdir(CLIENT_DIR):
+        os.makedirs(CLIENT_DIR)
+        logger.info(f'A directory "{CLIENT_DIR}" was created')
+        logger.info(f'Please supply the file to the "{CLIENT_DIR}" directory')
+        return False
+    return True
+
+
+def get_file() -> str:
+    """Get file from user input"""
+    while True:
+        file_name = input("File to transfer: ")
+        file_path = os.path.join(CLIENT_DIR, file_name)
+        if os.path.exists(file_path):
+            return file_path
+        logger.error(f"{file_name} doesn't exist in the {CLIENT_DIR} directory")
+
+
+def get_server() -> str:
+    """Get the server address from user input"""
+    return input("Server address: ")
+
+
+def connect(server: str) -> Optional[socket.socket]:
+    """Establish connection between server and client"""
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    try:
+        s.connect((server, PORT))
+        return s
+    except socket.error as e:
+        logger.info("Couldn't connect to a server...")
+        logger.debug(e)
+        return None
+
+
+def compute_file_hash(file_path: str, hash_algo="sha256") -> str:
+    """Compute hash of the file using the specified algorithm (default: SHA-256)."""
+    hash_func = hashlib.new(hash_algo)
+    with open(file_path, "rb") as file:
+        while chunk := file.read(4096):
+            hash_func.update(chunk)
+    return hash_func.hexdigest()
+
+
+def send_file(s: socket.socket, file_path: str) -> None:
+    """Attempt to send a file to the server"""
+    file_size = os.path.getsize(file_path)
+    file_hash = compute_file_hash(file_path)
+    total_sent = 0
+
+    s.send(file_path.encode() + b"\n")
+    s.send(str(file_size).encode() + b"\n")
+    s.send(file_hash.encode() + b"\n")
+
+    with open(file_path, "rb") as file:
+        with tqdm(
+            total=file_size, unit="B", unit_scale=True, desc="Sending file..."
+        ) as progress:
+            while data := file.read(CHUNK_BYTES):
+                s.sendall(data)
+                total_sent += len(data)
+                progress.update(len(data))
+
+    if total_sent == file_size:
+        logger.info("File transferred successfully!")
+    else:
+        logger.error("Something went wrong with the file transfer")
+
+
+def main():
+    while not check_directory():
+        time.sleep(10)
+
+    sock = None
+    server = get_server()
+    while True:
+        sock = connect(server)
+        if sock:
+            logger.info("Connected successfully!")
+            break
+        logger.info("Retrying in 2 seconds...")
+        time.sleep(2)
+
+    send_file(sock, get_file())
+    sock.close()
+
+
+if __name__ == "__main__":
+    main()
---
+++ server.py
@@ -0,0 +1,115 @@
+import hashlib
+import logging
+import os
+import socket
+import time
+from pathlib import Path
+
+from tqdm import tqdm
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+
+formatter = logging.Formatter("[%(name)s] %(asctime)s %(levelname)s - %(message)s")
+handler = logging.StreamHandler()
+logger.addHandler(handler)
+
+
+IP_ADDRESS = "127.0.0.1"
+PORT = 3254
+SERVER_DIR = "server"
+CHUNK_BYTES = 1024
+
+
+def is_file(file_path: str) -> bool:
+    """Check if a file exists"""
+    if os.path.exists(file_path):
+        return True
+    return False
+
+
+def get_size(file_path: str) -> int:
+    return os.path.getsize(file_path)
+
+
+def create_directory() -> None:
+    """Create a server directory, if it doesn't exist"""
+    if not os.path.isdir(SERVER_DIR):
+        os.makedirs(SERVER_DIR)
+        logger.info(f'A directory "{SERVER_DIR}" was created')
+
+
+def alter_file_path(file_path: str) -> str:
+    """Alter the file path to include the server designated folder"""
+    return os.path.join(SERVER_DIR, Path(file_path).name)
+
+
+def compute_file_hash(file_path: str, hash_algo="sha256") -> str:
+    """Compute hash of the file using the specified algorithm (default: SHA-256)."""
+    hash_func = hashlib.new(hash_algo)
+    with open(file_path, "rb") as file:
+        while chunk := file.read(4096):
+            hash_func.update(chunk)
+    return hash_func.hexdigest()
+
+
+def verify_hash(file_path: str, hash: str) -> bool:
+    """Compares the original hash with the hash of a file"""
+    return compute_file_hash(file_path) == hash
+
+
+def receive_file(client_socket: socket.socket) -> None:
+    """Receive a file from the client"""
+    client_file_path = client_socket.recv(CHUNK_BYTES).decode()
+    client_file_size = client_socket.recv(CHUNK_BYTES).decode()
+    client_file_hash = client_socket.recv(CHUNK_BYTES).decode()
+
+    server_file_path = alter_file_path(client_file_path)
+    # print(client_file_path + "\n" + client_file_size + "\n" + client_file_hash)
+    print(type(int(client_file_size)))
+
+    # TODO: Metadata comparison, send response back to the client
+    if (
+        is_file(server_file_path)
+        and get_size(server_file_path)
+        and verify_hash(server_file_path, client_file_hash)
+    ):
+        logger.warning("Identical file already exists")
+        return None
+
+        with open(file_save_path + file_name, "wb") as file:
+            with tqdm(
+                total=client_file_size,
+                unit="B",
+                unit_scale=True,
+                desc="Receiving file...",
+            ) as progress:
+                total_sent = 0
+                while chunks <= client_file_size:
+                    data = client.recv(CHUNK_BYTES)
+                    if not (data):
+                        break
+                    file.write(data)
+                    total_sent += len(data)
+                    progress.update(len(data))
+
+        if total_sent == file_size:
+            logger.info("File transferred successfully!")
+        else:
+            logger.error("Something went wrong with the file transfer")
+
+
+def main():
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.bind((IP_ADDRESS, PORT))
+    logger.info("Waiting for a connection...")
+    s.listen(0)
+    while True:
+        client_socket, client_address = s.accept()
+        logger.info(f"{client_address} connected to the server")
+        receive_file(client_socket)
+        client_socket.close()
+
+
+if __name__ == "__main__":
+    main()
